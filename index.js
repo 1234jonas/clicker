bb1f9c10fd07285092746edfd285a58806814358
jfkheoiuadklfjige <click.here.for.a.safe.download@gmail.com>
2024-07-21 19:59:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
+
+function setCookie(name, value) {
+
+    const date = new Date();
+    date.setFullYear(date.getFullYear() + 100);
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+
+}
+
+function getCookie(name) {
+
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+
+    }
+    return null;
+
+}
+
+function deleteCookie(name) {
+
+    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+}
+
+let score = parseInt(getCookie('score')) || 0;
+const scoretext = document.getElementById("score");
+scoretext.textContent = score;
+
+function clickmeClick() {
+
+    score++;
+    scoretext.textContent = score;
+    setCookie('score', score);
+
+}
+
+function deletescoreClick() {
+
+    deleteCookie('score');
+    score = 0;
+    scoretext.textContent = score;
+
+
+}
+
+document.getElementById("clickme").addEventListener('click', clickmeClick);
+document.getElementById("deletescore").addEventListener('click', deletescoreClick);
+
+})
